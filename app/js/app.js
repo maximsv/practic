f01a1ff46f7b3d7aa18d75aa3218251b6f2bf717
maximsv <maximsv1701@gmail.com>
2020-12-06 20:16:43 +0200

    responsive

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,24 @@
 // import $ from 'jquery' // import module example (npm i -D jquery)
+$(function(){
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-
-})
+	$('.top-line').after('<div class="mobile-menu d-lg-none">');
+	$('.top-menu').clone().appendTo('.mobile-menu');
+	$('.mobile-menu-button').click(function() {
+		$('.mobile-menu').stop().slideToggle();
+	});
 $('.test-popup-link').magnificPopup({
     type: 'image'
     // other options
 });
+
+
+		$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
+	});
+});
+
+
+
+
